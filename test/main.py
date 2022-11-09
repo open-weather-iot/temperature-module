59ba9c0ad871d4c942b18ef0db13bb7dba58eda6
Arthur Simas <arthursimas1@gmail.com>
2022-11-09 16:57:20 -0300

    refactoring SPI bus

--- test/main.py
+++ test/main.py
@@ -1,6 +1,7 @@
 from machine import Pin
 import time
 import utime
+from util.bus import SPI
 from src.display_nokia import DisplayNokia
 from src.max31865 import MAX31865
 
@@ -21,41 +22,40 @@ display = DisplayNokia()
 # ---------------------------------------
 
 def main():
-  # INITIAL TIMESTAMP
-  timestamp_initial = utime.ticks_us()
-  # TURN ON INTERNAL RASPBERRY PICO LED
-  led_internal.value(1)
-  
-  #LOOP: READ SENSORS
-  while True:
+    # INITIAL TIMESTAMP
+    timestamp_initial = utime.ticks_us()
+    # TURN ON INTERNAL RASPBERRY PICO LED
+    led_internal.value(1)
+    
+    #LOOP: READ SENSORS
+    while True:
+        # SENSORS MAPPING
+        # INSIDE LOOP TO RESET THE MODULE IN CASE OF PIN READING ERRORS
+        sensors = {
+          "t1": MAX31865(SPI(port=1)),
+          # "t2": MAX31865(SPI(port=2)),
+        }
 
-    # SENSORS MAPPING
-    # INSIDE LOOP TO RESET THE MODULE IN CASE OF PIN READING ERRORS
-    sensors = {
-      "t1": MAX31865(clk_pin=10, sdi_tx_pin = 11, sdo_rx_pin=12,  cs_pin=13, spi_id=1),
-      #"t2": MAX31865(clk_pin=10, sdi_tx_pin = 11, sdo_rx_pin=12,  cs_pin=13, spi_id=1)
-    }
+        print('-----------------------')
+        #Read Time
+        timestamp_actual = utime.ticks_us()
+        timepassed = (timestamp_actual - timestamp_initial ) /1000000
+        print('{:0.2f}s'.format(timepassed)) 
 
-    print('-----------------------')
-    #Read Time
-    timestamp_actual = utime.ticks_us()
-    timepassed = (timestamp_actual - timestamp_initial ) /1000000
-    print('{:0.2f}s'.format(timepassed)) 
+        #Read
+        read_map = {nome: sensor.read() for (nome, sensor) in sensors.items()}
+        print(read_map)
 
-    #Read
-    read_map = {nome: sensor.read() for (nome, sensor) in sensors.items()}
-    print(read_map)
+        #Display
+        temp = str(round(read_map['t1']['value'], 2))
+        resistance = str(round(read_map['t1']['raw']['resistance'],2))
+        scale = read_map['t1']['unit']
+        #adc = str(read_map['t1']['raw']['adc'])
+        display.display_temp(temp = temp, scale=scale, resistance=resistance, showAll=True)
+        display.lcd.fill(0)
 
-    #Display
-    temp = str(read_map['t1']['value'])
-    resistance = str(round(read_map['t1']['raw']['resistance'],2))
-    scale = read_map['t1']['unit']
-    #adc = str(read_map['t1']['raw']['adc'])
-    display.display_temp(temp = temp, scale=scale, resistance=resistance, showAll=True)
-    display.lcd.fill(0)
-
-    led_internal.toggle() #internal led toggle
-    time.sleep_ms(interval)
+        led_internal.toggle() #internal led toggle
+        time.sleep_ms(interval)
 
 #RUN
 main()
