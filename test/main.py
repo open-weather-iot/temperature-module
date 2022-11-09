39c295cba08a0492cfb4485975f42cab6f0b6ac2
Marcos Paulo Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2022-11-09 14:23:57 -0300

    Update main.py

--- test/main.py
+++ test/main.py
@@ -1,39 +1,43 @@
 from machine import Pin
 import time
 import utime
-from ..src.display_nokia import DisplayNokia
-from ..src.max31865 import MAX31865
+from src.display_nokia import DisplayNokia
+from src.max31865 import MAX31865
 
 # ---------------------------------------
 # ------------- SETUP    ----------------
 # ---------------------------------------
 
 # PORTS CONFIGURATION
-led_internal = Pin(25, Pin.OUT)
+led_internal = Pin(25,Pin.OUT) #INTERNAL LED
 
 # READ_INTERVAL (ms) 
 interval = 1000
 
-# SENSORS MAPPING
-sensors = {
-  "t1": MAX31865(clk_pin=10, sdi_tx_pin = 11, sdo_rx_pin=12,  cs_pin=13, spi_id=1)
-  #"t2": MAX31865(clk_pin=10, sdi_tx_pin = 11, sdo_rx_pin=12,  cs_pin=13, spi_id=1)
-}
-
+# DISPLAY CLASS
 display = DisplayNokia()
-
 # ---------------------------------------
 # -------------   LOOP   ----------------
 # ---------------------------------------
 
 def main():
-  # INITIAL TIMESTAMP TIME
+  # INITIAL TIMESTAMP
   timestamp_initial = utime.ticks_us()
+  # TURN ON INTERNAL RASPBERRY PICO LED
   led_internal.value(1)
-
+  
+  #LOOP: READ SENSORS
   while True:
+
+    # SENSORS MAPPING
+    # INSIDE LOOP TO RESET THE MODULE IN CASE OF PIN READING ERRORS
+    sensors = {
+      "t1": MAX31865(clk_pin=10, sdi_tx_pin = 11, sdo_rx_pin=12,  cs_pin=13, spi_id=1),
+      #"t2": MAX31865(clk_pin=10, sdi_tx_pin = 11, sdo_rx_pin=12,  cs_pin=13, spi_id=1)
+    }
+
     print('-----------------------')
-    #Tempo
+    #Read Time
     timestamp_actual = utime.ticks_us()
     timepassed = (timestamp_actual - timestamp_initial ) /1000000
     print('{:0.2f}s'.format(timepassed)) 
@@ -50,9 +54,8 @@ def main():
     display.display_temp(temp = temp, scale=scale, resistance=resistance, showAll=True)
     display.lcd.fill(0)
 
-    #display.show_display_message(temperatura, timepassed)
     led_internal.toggle() #internal led toggle
     time.sleep_ms(interval)
 
-if __name__ == "__main__":
-  main()
+#RUN
+main()
